3727a04ad783c5ede762741c6b3859c5d3dabc67
Kahraman Boyunegmez <kahramanboyunegmez@gmail.com>
2023-09-13 23:36:37 +0300

    .

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,13 +4,10 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\OrdersController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-
 Route::group(['middleware' => ['auth:sanctum']], function () {
-
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::group(['prefix' => '/orders', 'as' => 'orders.'], function () {
