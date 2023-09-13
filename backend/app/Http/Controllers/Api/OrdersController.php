a0001f5761ca476dcbbde5a5187eb226267790ff
Kahraman Boyunegmez <kahramanboyunegmez@gmail.com>
2023-09-13 20:21:07 +0300

    .

--- backend/app/Http/Controllers/Api/OrdersController.php
+++ backend/app/Http/Controllers/Api/OrdersController.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Http\Resources\OrderCollection;
 use App\Models\Order;
 
 /**
  * Class OrdersController
  * @package App\Http\Controllers\Api
  */
-class OrdersController extends Controller
+class OrdersController extends BaseController
 {
     public function index()
     {
-        return Order::all();
+        $orders = Order::all();
+
+        return new OrderCollection($orders);
     }
 }
---
+++ backend/app/Http/Resources/OrderResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class OrderResource
+ * @parent App\Http\Resources
+ */
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'load_type' => $this->load_type,
+            'commodity' => $this->commodity,
+            'departure_city' => $this->departure_city,
+            'arrival_city' => $this->arrival_city,
+        ];
+    }
+}
