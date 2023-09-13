91139f31e5ed881fb605080ca7ca1d58fdebf57a
Kahraman Boyunegmez <kahramanboyunegmez@gmail.com>
2023-09-13 21:16:26 +0300

    .

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,7 @@
 import { Col, Dropdown, DropdownButton, Form, InputGroup } from "react-bootstrap";
 import React, { useState } from "react";
-import useCustomization from "../hooks/useCustomization";
 
 const Sidebar = ({ selectedSources, selectedDate, selectedCategory }) => {
-	const { sources, categories } = useCustomization();
 	const [selectDate, setSelectDate] = useState("");
 
 	const handleSourceChange = (event) => {
@@ -28,18 +26,18 @@ const Sidebar = ({ selectedSources, selectedDate, selectedCategory }) => {
 						<Form.Control type="date" max={new Date().toISOString().split("T")[0]} value={selectDate} onChange={handleDate} />
 					</Form.Group>
 
-					<h5 className="pt-3">Categories</h5>
-					<DropdownButton as={InputGroup.Append} variant="outline-secondary" title="Choose Category" onSelect={handleCategory}>
-						<Dropdown.Item eventKey="">All Categories</Dropdown.Item>
-						{categories &&
-							categories.map((category, index) => {
-								return (
-									<Dropdown.Item key={index} eventKey={category.category}>
-										{category.category}
-									</Dropdown.Item>
-								);
-							})}
-					</DropdownButton>
+					{/*<h5 className="pt-3">Categories</h5>*/}
+					{/*<DropdownButton as={InputGroup.Append} variant="outline-secondary" title="Choose Category" onSelect={handleCategory}>*/}
+					{/*	<Dropdown.Item eventKey="">All Categories</Dropdown.Item>*/}
+					{/*	{categories &&*/}
+					{/*		categories.map((category, index) => {*/}
+					{/*			return (*/}
+					{/*				<Dropdown.Item key={index} eventKey={category.category}>*/}
+					{/*					{category.category}*/}
+					{/*				</Dropdown.Item>*/}
+					{/*			);*/}
+					{/*		})}*/}
+					{/*</DropdownButton>*/}
 				</div>
 			</div>
 		</Col>
