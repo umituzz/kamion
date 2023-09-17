47dfecc6a57699e0f2dee66ac883bc786e6f23b2
Kahraman Boyunegmez <kahramanboyunegmez@gmail.com>
2023-09-17 11:35:03 +0300

    .

---
+++ backend/tests/Feature/Http/Controllers/Auth/AuthenticatedSessionControllerTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+/**
+ * Class AuthenticatedSessionControllerTest
+ * @package Tests\Feature\Http\Controllers\Auth
+ * @coversDefaultClass \App\Http\Controllers\Auth\AuthenticatedSessionController
+ */
+class AuthenticatedSessionControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_login_screen_can_be_rendered(): void
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_users_can_authenticate_using_the_login_screen(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function test_users_can_not_authenticate_with_invalid_password(): void
+    {
+        $user = User::factory()->create();
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'wrong-password',
+        ]);
+
+        $this->assertGuest();
+    }
+
+    public function test_logout()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->post('/logout');
+
+        $response->assertRedirect('/');
+        $this->assertGuest();
+    }
+
+    public function test_logout_failure()
+    {
+        $response = $this->post('/logout');
+
+        $response->assertStatus(302);
+        $this->assertGuest();
+    }
+
+
+}
